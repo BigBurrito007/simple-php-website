6436691436500e2ae63b7527736622b346aed569
rocioferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2021-06-04 10:35:05 -0300

    image try

--- includes/functions.php
+++ includes/functions.php
@@ -72,7 +72,7 @@ function page_content()
 
     echo file_get_contents($path);
     echo ' 
-    <img src="https://aws.revistavanityfair.es/prod/designs/v1/assets/785x589/47441.jpg" class="img" width="280" height="125" alt="Frineds Banner" />
+    <img src="https://i.blogs.es/2b1b39/friends/1366_521.jpeg" class="img" alt="Frineds Banner" />
     ';
 }
 
