3a6782deb857dc3ff25de3384f58fd2e2525a2b7
Cristian Basanta <44707998+BigBurrito007@users.noreply.github.com>
2021-06-03 22:50:58 -0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,18 +8,11 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Friends Chatbot',
         'site_url' => '',
         'pretty_uri' => false,
-        'nav_menu' => [
-            '' => 'Home',
-            'about-us' => 'About Us',
-            'products' => 'Products',
-            'contact' => 'Contact',
-        ],
         'template_path' => 'template',
-        'content_path' => 'content',
-        'version' => 'v3.1',
+   
     ];
 
     return isset($config[$key]) ? $config[$key] : null;
