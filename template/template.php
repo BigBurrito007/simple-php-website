a137a6a2fc9348173b0f66c30e56d86c317ef0a9
basanta <basanta@ibm.com>
2021-06-04 10:33:37 -0300

    s

--- template/template.php
+++ template/template.php
@@ -4,26 +4,23 @@
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <title><?php page_title(); ?> | <?php site_name(); ?></title>
-    <link href="<?php site_url(); ?>/template/style.css" rel="stylesheet" type="text/css" /> 
-</head>
+    <title>FRIENDS</title>
+   
 <body>
 <div class="wrap">
 
     <header>
-        <h1><?php site_name(); ?></h1>
-        <nav class="menu">
-            <?php nav_menu(); ?>
-        </nav>
+        <h1>CHATEA EN LA MEJOR PAGINA QUE VISTE</h1>
+        
     </header>
 
     <article>
-        <h2><?php page_title(); ?></h2>
-        <?php page_content(); ?>
+        <h2></h2>
+        
     </article>
 
     <footer>
-        <small>&copy;<?php echo date('Y'); ?> <?php site_name(); ?>.<br><?php site_version(); ?></small>
+        
     </footer>
 
 </div>
