fda3dc54801b1de416940c9d139945df8e48c320
BigBurrito007 <44707998+BigBurrito007@users.noreply.github.com>
2021-06-05 22:24:30 -0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+<style>
+  .chatElement {
+    height: 600px;
+    width: 100%;
+  }
+</style>
+</head>
+<body>
+<div class="chatElement"></div>
+<script>
+  const element = document.querySelector('.chatElement');
+  window.watsonAssistantChatOptions = {
+    integrationID: "7de74f70-6bf3-40bf-90a9-d2b6f73531b5", // The ID of this integration.
+    region: "us-south", // The region your integration is hosted in.
+    serviceInstanceID: "5f315828-90e0-4a72-a079-7510afe29988", 
+    // Provide the custom element.
+    element: element,
+    // Hide the close button since we want it always open.
+    hideCloseButton: true,
+    // Hide the default launcher.
+    showLauncher: false,
+    // Make the window open by default.
+    openChatByDefault: true,
+
+    onLoad: function(instance) {
+      instance.render();
+    }
+  };
+
+  setTimeout(function(){const t=document.createElement('script');t.src='https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js';document.head.appendChild(t);});
+</script>
+</body>
+</html>
